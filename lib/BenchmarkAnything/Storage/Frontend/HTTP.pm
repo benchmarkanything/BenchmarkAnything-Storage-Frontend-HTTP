ee3ed91e9e6dd0a9ca36231b74ebc001e43089d1
Steffen Schwigon <ss5@renormalist.net>
2015-08-28 12:30:48 +0200

    refactoring
    ...to use BenchmarkAnything::Storage::Frontend::Lib.

--- lib/BenchmarkAnything/Storage/Frontend/HTTP.pm
+++ lib/BenchmarkAnything/Storage/Frontend/HTTP.pm
@@ -5,22 +5,15 @@ package BenchmarkAnything::Storage::Frontend::HTTP;
 
 use Mojo::Base 'Mojolicious';
 
+require BenchmarkAnything::Storage::Frontend::Lib;
 require File::HomeDir; # MUST 'require', 'use' conflicts with Mojolicious
 require Tapper::Benchmark;
 require File::Slurp;
 require YAML::Any;
 require DBI;
 
-# config
-my $configfile  = $ENV{BENCHMARKANYTHING_CONFIGFILE} || File::HomeDir->my_home . "/.benchmarkanything.cfg";
-my $config      = YAML::Any::Load("".File::Slurp::read_file($configfile));
-
-# connection
-my $dsn              = $config->{benchmarkanything}{backends}{tapper}{benchmark}{dsn};
-my $user             = $config->{benchmarkanything}{backends}{tapper}{benchmark}{user};
-my $password         = $config->{benchmarkanything}{backends}{tapper}{benchmark}{password};
-my $dbh              = DBI->connect ($dsn, $user, $password, { RaiseError => 1, AutoCommit => 1 });
-my $tapper_benchmark = Tapper::Benchmark->new ({ dbh => $dbh, debug => 0 });
+my $balib            = BenchmarkAnything::Storage::Frontend::Lib->new->connect;
+my $tapper_benchmark = Tapper::Benchmark->new ({ dbh => $balib->{dbh}, debug => 0 });
 
 # This method will run once at server start
 sub startup {
@@ -28,8 +21,7 @@ sub startup {
 
         $self->plugin('InstallablePaths');
 
-        # Normal route to controller
-        # # helper
+        # helper
         $self->helper (tapper_benchmark => sub { $tapper_benchmark } );
 
         # routes
